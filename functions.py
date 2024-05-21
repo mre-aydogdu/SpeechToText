54f0d90b18a1dcfb6b59e27ff9a2d9716e45e83b
Emre <mre.aydogdu@gmail.com>
2024-05-22 00:17:59 +0300

    enhance button and path added

--- functions.py
+++ functions.py
@@ -1,18 +1,24 @@
-import json
-import wave
-import os
-from tkinter import filedialog, END
+import string
+from tkinter import filedialog
+
 import whisper
-import subprocess
-import soundfile as sf
-from scipy.signal import resample
-from vosk import Model, KaldiRecognizer
 
 model_whisper = whisper.load_model("base")
 model_vosk = "vosk-model-small-tr-0.3"
-
+from df.enhance import enhance, init_df, load_audio, save_audio
 import subprocess
 from tkinter import END
+
+
+def normalize_text(text):
+    # Convert all capital letters to lowercase
+    normalized_text = text.lower()
+
+    # Remove punctuation using translation and str.maketrans
+    translator = str.maketrans("", "", string.punctuation)
+    normalized_text = normalized_text.translate(translator)
+
+    return normalized_text
 
 
 def VoskTranscribe(entry_file_path, textbox, language='tr'):
@@ -37,7 +43,7 @@ def VoskTranscribe(entry_file_path, textbox, language='tr'):
     try:
         with open(output_file, "r") as f:
             vosk_text = f.read()
-            textbox.configure(state="normal")
+            textbox.configure(state="disabled")
             textbox.delete("1.0", END)
             textbox.insert("1.0", vosk_text)
             textbox.configure(state="disabled")
@@ -52,29 +58,63 @@ def WhisperTranscribe(entry_file_path, textbox):
         print(f"Transcribing with Whisper: {filename}")
         result = model_whisper.transcribe(filename)
         print("Transcription:", result["text"])
-
+        normalized_text = normalize_text(result["text"])
         # Display the transcription in the specified textbox
-        textbox.configure(state="normal")
+        textbox.configure(state="disabled")
         textbox.delete("1.0", END)  # Clear any previous content
-        textbox.insert("1.0", result["text"])  # Insert the transcription text
+        textbox.insert("1.0", normalized_text)  # Insert the transcription text
         textbox.configure(state="disabled")  # Disable editing of the textbox
     else:
         print("No file selected")
 
 
-
-def Enhance(entry_file_path):
+def Enhance(entry_file_path, enhanced_file_path):
     filename = entry_file_path.get()
-    print(f"Enhancing: {filename}")
+    if filename:
+        print(f"Enhancing audio: {filename}")
+
+        # Initialize DeepFilter model and state
+        model, df_state, _ = init_df()
+
+        # Load the audio file
+        audio, _ = load_audio(filename, sr=df_state.sr())
+
+        # Enhance the audio using DeepFilter
+        enhanced_audio = enhance(model, df_state, audio)
+
+        # Save the enhanced audio to a new file
+        output_filename = "enhanced_audio.wav"
+        save_audio(output_filename, enhanced_audio, df_state.sr())
+
+        enhanced_file_path.configure(state="normal")
+        enhanced_file_path.delete(0, END)
+        enhanced_file_path.insert(0, output_filename)
+        enhanced_file_path.configure(state="readonly")
+
+        print("Audio enhancement completed.")
+    else:
+        print("No file selected")
 
 
 def UploadAction(entry_file_path, event=None):
     filename = filedialog.askopenfilename(
         filetypes=[("MP3 files", "*.mp3"), ("M4A files", "*.m4a"), ("WAV files", "*.wav")])
+    entry_file_path.configure(state="normal")
     entry_file_path.delete(0, END)
     entry_file_path.insert(0, filename)
+    entry_file_path.configure(state="readonly")
+
     print(f'Selected: {filename}')
 
 
-def ResetPath(entry_file_path):
-    entry_file_path.delete(0, END)
+def ResetPath(entry_file_path, enhanced_file_path, reset_type):
+    if reset_type == "entry":
+        entry_file_path.configure(state="normal")
+        entry_file_path.delete(0, END)
+        entry_file_path.configure(state="readonly")
+    elif reset_type == "enhanced":
+        enhanced_file_path.configure(state="normal")
+        enhanced_file_path.delete(0, END)
+        enhanced_file_path.configure(state="readonly")
+    else:
+        print("Invalid target specified for reset")
--- ui.py
+++ ui.py
@@ -15,6 +15,12 @@ def create_ui(app):
     entry_file_path = CTkEntry(master=app, width=400, fg_color="#111111", border_width=0, text_color="white",
                                font=("default", 10), state="normal")
     entry_file_path.place(relx=0.5, rely=0.1, anchor=CENTER)
+    entry_file_path.configure(state="readonly")
+
+    enhanced_file_path = CTkEntry(master=app, width=400, fg_color="#111111", border_width=0, text_color="white",
+                                  font=("default", 10), state="normal")
+    enhanced_file_path.place(relx=0.5, rely=0.13, anchor=CENTER)
+    enhanced_file_path.configure(state="readonly")
 
     # Define the custom font
     custom_font = CTkFont(family="Montserrat", size=14, weight="bold")
@@ -25,26 +31,31 @@ def create_ui(app):
     # Use CTkButton instead of tkinter Button
     button_whisper = CTkButton(master=app, text="Transcribe with Whisper", width=200, hover_color="red",
                                command=lambda: WhisperTranscribe(entry_file_path, whisper_textbox), font=custom_font)
-    button_whisper.place(relx=0.25, rely=0.4, anchor=CENTER)
+    button_whisper.place(relx=0.25, rely=0.3, anchor=CENTER)
 
     button_vosk = CTkButton(master=app, text="Transcribe with Vosk", width=200, hover_color="orange",
                             command=lambda: VoskTranscribe(entry_file_path, vosk_textbox), font=custom_font)
-    button_vosk.place(relx=0.75, rely=0.4, anchor=CENTER)
+    button_vosk.place(relx=0.75, rely=0.3, anchor=CENTER)
 
     # Add a Reset button to clear the selected file and path
     reset_button = CTkButton(master=app, image=cross_image, text='', width=30, fg_color="#1E201F",
-                             command=lambda: ResetPath(entry_file_path))
+                             command=lambda: ResetPath(entry_file_path, enhanced_file_path, "entry"))
     reset_button.place(relx=0.635, rely=0.1, anchor=CENTER)
+
+    reset_button_2 = CTkButton(master=app, image=cross_image, text='', width=30, fg_color="#1E201F",
+                               command=lambda: ResetPath(entry_file_path, enhanced_file_path, "enhanced"))
+    reset_button_2.place(relx=0.635, rely=0.13, anchor=CENTER)
+
     button_enhance = CTkButton(master=app, text='Enhance', fg_color="purple",
-                               command=lambda: Enhance(entry_file_path), font=custom_font)
-    button_enhance.place(relx=0.5, rely=0.35, anchor=CENTER)
+                               command=lambda: Enhance(entry_file_path, enhanced_file_path), font=custom_font)
+    button_enhance.place(relx=0.69, rely=0.13, anchor=CENTER)
 
-    vosk_textbox = CTkTextbox(master=app, width=400, height=100, fg_color="#111111", border_width=0,
+    vosk_textbox = CTkTextbox(master=app, width=400, height=300, fg_color="#111111", border_width=0,
                               text_color="white", font=("default", 20), state="normal")
-    vosk_textbox.place(relx=0.75, rely=0.6, anchor=CENTER)
-
-    whisper_textbox = CTkTextbox(master=app, width=400, height=100, fg_color="#111111", border_width=0,
+    vosk_textbox.place(relx=0.75, rely=0.5, anchor=CENTER)
+    vosk_textbox.configure(state="disabled")
+    whisper_textbox = CTkTextbox(master=app, width=400, height=300, fg_color="#111111", border_width=0,
                                  text_color="white", font=("default", 20), state="normal")
-    whisper_textbox.place(relx=0.25, rely=0.6, anchor=CENTER)
-
+    whisper_textbox.place(relx=0.25, rely=0.5, anchor=CENTER)
+    whisper_textbox.configure(state="disabled")
     return entry_file_path, vosk_textbox, whisper_textbox
