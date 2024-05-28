9780ef1e441255009c1ba47e051a88249fedb528
Emre <mre.aydogdu@gmail.com>
2024-05-28 09:50:14 +0300

    Similarity Implementation beta

--- functions.py
+++ functions.py
@@ -1,13 +1,42 @@
+import os
 import string
 from tkinter import filedialog
-
+import ffmpeg
 import whisper
-
-model_whisper = whisper.load_model("base")
-model_vosk = "vosk-model-small-tr-0.3"
 from df.enhance import enhance, init_df, load_audio, save_audio
 import subprocess
 from tkinter import END
+
+model_whisper = whisper.load_model("base")
+model_vosk = "vosk-model-small-tr-0.3"
+
+
+def read_text_file(filename):
+    try:
+        with open(filename, 'r', encoding='utf-8') as file:
+            return file.read().lower()  # Convert text to lowercase
+    except FileNotFoundError:
+        print(f"Error: File '{filename}' not found.")
+        return None
+    except Exception as e:
+        print(f"Error reading file '{filename}': {e}")
+        return None
+
+
+def calculate_similarity(text1, text2):
+    if text1 is None or text2 is None:
+        return -1
+    else:
+        words1 = set(text1.split())
+        words2 = set(text2.split())
+
+        print(f'Text1 words: {words1}')  # Debug print
+        print(f'Text2 words: {words2}')  # Debug print
+
+        common_words = words1 & words2
+        similarity = (len(common_words) / max(len(words1), len(words2))) * 100
+        return similarity
+
 
 
 def normalize_text(text):
@@ -15,14 +44,12 @@ def normalize_text(text):
     normalized_text = text.lower()
 
     # Remove punctuation using translation and str.maketrans
-    translator = str.maketrans("", "", string.punctuation)
-    normalized_text = normalized_text.translate(translator)
-
-    return normalized_text
+    return text.lower().translate(str.maketrans("", "", string.punctuation))
 
 
-def VoskTranscribe(entry_file_path, textbox, language='tr'):
+def VoskTranscribe(entry_file_path, textbox, similarity_label, original_text, language='tr'):
     filename = entry_file_path.get()
+
     if not filename:
         print("No file selected")
         return
@@ -43,33 +70,86 @@ def VoskTranscribe(entry_file_path, textbox, language='tr'):
     try:
         with open(output_file, "r") as f:
             vosk_text = f.read()
-            textbox.configure(state="disabled")
+            textbox.configure(state="normal")
             textbox.delete("1.0", END)
             textbox.insert("1.0", vosk_text)
             textbox.configure(state="disabled")
             print("Vosk Transcription Result:", vosk_text)
+
+            print("Before similarity calculation")  # Debug print
+            similarity_percentage = calculate_similarity(original_text,
+                                                         vosk_text)  # Ensure correct variables are passed
+            print(f'Similarity Percentage: {similarity_percentage:.2f}%')
+            print("After similarity calculation")  # Debug print
+
+            similarity_label.configure(text=f"Vosk Similarity: {similarity_percentage:.2f}%")
+
     except Exception as e:
         print(f"An error occurred while reading the transcription file: {e}")
 
 
-def WhisperTranscribe(entry_file_path, textbox):
+def WhisperTranscribe(entry_file_path, textbox, similarity_label, original_text):
     filename = entry_file_path.get()
-    if filename:
-        print(f"Transcribing with Whisper: {filename}")
-        result = model_whisper.transcribe(filename)
-        print("Transcription:", result["text"])
+
+    if not filename:
+        print("No file selected")
+        return
+
+    # Convert the input file to WAV using ffmpeg
+    converted_filename = './cache/converted_audio.wav'
+    try:
+        (
+            ffmpeg
+            .input(filename)
+            .output(converted_filename)
+            .run(overwrite_output=True)
+        )
+    except ffmpeg.Error as e:
+        print(f"FFmpeg error: {e}")
+        return
+
+    if not os.path.isfile(converted_filename):
+        print(f"Converted file does not exist: {converted_filename}")
+        return
+
+    print(f"Transcribing with Whisper: {filename}")
+
+    try:
+        result = model_whisper.transcribe(converted_filename)
         normalized_text = normalize_text(result["text"])
+
         # Display the transcription in the specified textbox
-        textbox.configure(state="disabled")
+        textbox.configure(state="normal")
         textbox.delete("1.0", END)  # Clear any previous content
         textbox.insert("1.0", normalized_text)  # Insert the transcription text
         textbox.configure(state="disabled")  # Disable editing of the textbox
-    else:
-        print("No file selected")
+
+        similarity = calculate_similarity(normalized_text, original_text)
+        similarity_label.configure(text=f"Whisper Similarity: {similarity:.2f}%")
+
+        print("Whisper Transcription Result:", normalized_text)
+    except Exception as e:
+        print(f"An error occurred during Whisper transcription: {e}")
 
 
 def Enhance(entry_file_path, enhanced_file_path):
     filename = entry_file_path.get()
+
+    # Ensure the cache directory exists
+    cache_dir = './cache/'
+    if not os.path.exists(cache_dir):
+        os.makedirs(cache_dir)
+
+    # Convert the audio file to WAV format using ffmpeg
+    converted_filename = os.path.join(cache_dir, 'converted_audio.wav')
+    ffmpeg_command = ['ffmpeg', '-y', '-i', filename, converted_filename]
+    try:
+        subprocess.run(ffmpeg_command, check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error during ffmpeg conversion: {e}")
+        return
+
+    filename = converted_filename
     if filename:
         print(f"Enhancing audio: {filename}")
 
@@ -107,7 +187,26 @@ def UploadAction(entry_file_path, event=None):
     print(f'Selected: {filename}')
 
 
-def ResetPath(entry_file_path, enhanced_file_path, reset_type):
+def UploadAction_Text(entry_file_path, event=None):
+
+    filename = filedialog.askopenfilename(
+        filetypes=[("Text files", "*.txt"), ("All files", "*.*")])
+    entry_file_path.configure(state="normal")
+    entry_file_path.delete(0, END)
+    entry_file_path.insert(0, filename)
+    entry_file_path.configure(state="readonly")
+
+    print(f'Selected: {filename}')
+
+    # Read the text file and store its content
+    original_text = read_text_file(filename)
+    if original_text:
+        print(f'Original text loaded: {original_text[:100]}...')  # Display the first 100 characters as a preview
+    else:
+        print('Failed to load the original text.')
+
+
+def ResetPath(entry_file_path, enhanced_file_path, original_text_path, reset_type):
     if reset_type == "entry":
         entry_file_path.configure(state="normal")
         entry_file_path.delete(0, END)
@@ -116,5 +215,9 @@ def ResetPath(entry_file_path, enhanced_file_path, reset_type):
         enhanced_file_path.configure(state="normal")
         enhanced_file_path.delete(0, END)
         enhanced_file_path.configure(state="readonly")
+    elif reset_type == "original_path":
+        original_text_path.configure(state="normal")
+        original_text_path.delete(0, END)
+        original_text_path.configure(state="readonly")
     else:
         print("Invalid target specified for reset")
--- ui.py
+++ ui.py
@@ -2,60 +2,125 @@ from customtkinter import *
 from customtkinter import CTkEntry
 from PIL import Image
 
-from functions import VoskTranscribe, WhisperTranscribe, UploadAction, ResetPath, Enhance
+from functions import VoskTranscribe, WhisperTranscribe, UploadAction, ResetPath, Enhance, read_text_file, \
+    UploadAction_Text
 
 entry_file_path = None
-
+original_text_path = ""
 
 def create_ui(app):
     # Add a text box to show the file name and path
     global entry_file_path
+    global original_text_path
+    custom_font = CTkFont(family="Montserrat", size=14, weight="bold")
     cross_image = CTkImage(Image.open("Icons/close.png").resize((20, 20), Image.Resampling.LANCZOS))
 
-    entry_file_path = CTkEntry(master=app, width=400, fg_color="#111111", border_width=0, text_color="white",
+    original_text_path = CTkEntry(master=app, width=400, fg_color="#111111", border_width=0, text_color="white",
+                                  font=("default", 10), state="normal")
+    original_text_path.place(relx=0.5, rely=0.05, anchor=CENTER)
+    original_text = read_text_file(original_text_path.get())
+
+    button_open_origin = CTkButton(master=app, text='Load Text', command=lambda: UploadAction_Text(original_text_path),
+                                   font=custom_font)
+    button_open_origin.place(relx=0.69, rely=0.05, anchor=CENTER, relwidth=0.09)
+
+    reset_button_original = CTkButton(master=app, image=cross_image, text='', fg_color="#1E201F",
+                                      command=lambda: ResetPath(entry_file_path, enhanced_file_path, original_text_path,
+                                                                "original_path"))
+    reset_button_original.place(relx=0.635, rely=0.05, anchor=CENTER, relwidth=0.02)
+
+    entry_file_path = CTkEntry(master=app, fg_color="#111111", border_width=0, text_color="white",
                                font=("default", 10), state="normal")
-    entry_file_path.place(relx=0.5, rely=0.1, anchor=CENTER)
+    entry_file_path.place(relx=0.5, rely=0.1, anchor=CENTER, relwidth=0.4)
     entry_file_path.configure(state="readonly")
 
-    enhanced_file_path = CTkEntry(master=app, width=400, fg_color="#111111", border_width=0, text_color="white",
+    enhanced_file_path = CTkEntry(master=app, fg_color="#111111", border_width=0, text_color="white",
                                   font=("default", 10), state="normal")
-    enhanced_file_path.place(relx=0.5, rely=0.13, anchor=CENTER)
+    enhanced_file_path.place(relx=0.5, rely=0.13, anchor=CENTER, relwidth=0.4)
     enhanced_file_path.configure(state="readonly")
 
     # Define the custom font
-    custom_font = CTkFont(family="Montserrat", size=14, weight="bold")
+
     button_open = CTkButton(master=app, text='Load', command=lambda: UploadAction(entry_file_path),
                             font=custom_font)
-    button_open.place(relx=0.69, rely=0.1, anchor=CENTER)
+    button_open.place(relx=0.69, rely=0.1, anchor=CENTER, relwidth=0.09)
+
+    whisper_button_enhance = CTkButton(master=app, text='Transcribe Enhanced File with Whisper', fg_color="purple",
+                                       command=lambda: WhisperTranscribe(enhanced_file_path, whisper_textbox_enhanced,
+                                                                         whisper_similarity_label_enhanced,
+                                                                         original_text), font=custom_font)
+    whisper_button_enhance.place(relx=0.25, rely=0.69, anchor=CENTER, relwidth=0.4)
 
     # Use CTkButton instead of tkinter Button
-    button_whisper = CTkButton(master=app, text="Transcribe with Whisper", width=200, hover_color="red",
-                               command=lambda: WhisperTranscribe(entry_file_path, whisper_textbox), font=custom_font)
-    button_whisper.place(relx=0.25, rely=0.3, anchor=CENTER)
+    button_whisper = CTkButton(master=app, text="Transcribe with Whisper", hover_color="red",
+                               command=lambda: WhisperTranscribe(entry_file_path, whisper_textbox,
+                                                                 whisper_similarity_label, original_text),
+                               font=custom_font)
+    button_whisper.place(relx=0.25, rely=0.3, anchor=CENTER, relwidth=0.4)
 
-    button_vosk = CTkButton(master=app, text="Transcribe with Vosk", width=200, hover_color="orange",
-                            command=lambda: VoskTranscribe(entry_file_path, vosk_textbox), font=custom_font)
-    button_vosk.place(relx=0.75, rely=0.3, anchor=CENTER)
+    vosk_button_enhance = CTkButton(master=app, text='Transcribe Enhanced File with Vosk', fg_color="purple",
+                                    command=lambda: VoskTranscribe(enhanced_file_path, vosk_textbox_enhanced,
+                                                                   vosk_similarity_label_enhanced, original_text),
+                                    font=custom_font)
+    vosk_button_enhance.place(relx=0.75, rely=0.69, anchor=CENTER, relwidth=0.4)
+
+    button_vosk = CTkButton(master=app, text="Transcribe with Vosk", hover_color="orange",
+                            command=lambda: VoskTranscribe(entry_file_path, vosk_textbox,
+                                                           vosk_similarity_label, original_text),
+                            font=custom_font)
+    button_vosk.place(relx=0.75, rely=0.3, anchor=CENTER, relwidth=0.4)
 
     # Add a Reset button to clear the selected file and path
-    reset_button = CTkButton(master=app, image=cross_image, text='', width=30, fg_color="#1E201F",
-                             command=lambda: ResetPath(entry_file_path, enhanced_file_path, "entry"))
-    reset_button.place(relx=0.635, rely=0.1, anchor=CENTER)
-
-    reset_button_2 = CTkButton(master=app, image=cross_image, text='', width=30, fg_color="#1E201F",
-                               command=lambda: ResetPath(entry_file_path, enhanced_file_path, "enhanced"))
-    reset_button_2.place(relx=0.635, rely=0.13, anchor=CENTER)
-
+    reset_button = CTkButton(master=app, image=cross_image, text='', fg_color="#1E201F",
+                             command=lambda: ResetPath(entry_file_path, enhanced_file_path, original_text_path,
+                                                       "entry"))
+    reset_button.place(relx=0.635, rely=0.1, anchor=CENTER, relwidth=0.02)
+    reset_button_2 = CTkButton(master=app, image=cross_image, text='', fg_color="#1E201F",
+                               command=lambda: ResetPath(entry_file_path, enhanced_file_path, original_text_path,
+                                                         "enhanced"))
+    reset_button_2.place(relx=0.635, rely=0.13, anchor=CENTER, relwidth=0.02)
     button_enhance = CTkButton(master=app, text='Enhance', fg_color="purple",
                                command=lambda: Enhance(entry_file_path, enhanced_file_path), font=custom_font)
-    button_enhance.place(relx=0.69, rely=0.13, anchor=CENTER)
+    button_enhance.place(relx=0.69, rely=0.13, anchor=CENTER, relwidth=0.092)
 
-    vosk_textbox = CTkTextbox(master=app, width=400, height=300, fg_color="#111111", border_width=0,
+    vosk_textbox = CTkTextbox(master=app, fg_color="#111111", border_width=0,
                               text_color="white", font=("default", 20), state="normal")
-    vosk_textbox.place(relx=0.75, rely=0.5, anchor=CENTER)
+    vosk_textbox.place(relx=0.75, rely=0.46, anchor=CENTER, relwidth=0.4, relheight=0.25)
     vosk_textbox.configure(state="disabled")
-    whisper_textbox = CTkTextbox(master=app, width=400, height=300, fg_color="#111111", border_width=0,
+
+    whisper_textbox = CTkTextbox(master=app, fg_color="#111111", border_width=0,
                                  text_color="white", font=("default", 20), state="normal")
-    whisper_textbox.place(relx=0.25, rely=0.5, anchor=CENTER)
+    whisper_textbox.place(relx=0.25, rely=0.46, anchor=CENTER, relwidth=0.4, relheight=0.25)
     whisper_textbox.configure(state="disabled")
-    return entry_file_path, vosk_textbox, whisper_textbox
+
+    whisper_textbox_enhanced = CTkTextbox(master=app, fg_color="#111111", border_width=0,
+                                          text_color="white", font=("default", 20), state="normal")
+    whisper_textbox_enhanced.place(relx=0.25, rely=0.85, anchor=CENTER, relwidth=0.4, relheight=0.25)
+    whisper_textbox_enhanced.configure(state="disabled")
+
+    vosk_textbox_enhanced = CTkTextbox(master=app, fg_color="#111111", border_width=0,
+                                       text_color="white", font=("default", 20), state="normal")
+    vosk_textbox_enhanced.place(relx=0.75, rely=0.85, anchor=CENTER, relwidth=0.4, relheight=0.25)
+    vosk_textbox_enhanced.configure(state="disabled")
+
+    # Add labels to display similarity percentages
+    whisper_similarity_label = CTkLabel(master=app, text="Whisper Similarity: N/A", font=custom_font)
+    whisper_similarity_label.place(relx=0.25, rely=0.55, anchor=CENTER)
+
+    vosk_similarity_label = CTkLabel(master=app, text="Vosk Similarity: N/A", font=custom_font)
+    vosk_similarity_label.place(relx=0.75, rely=0.55, anchor=CENTER)
+
+    whisper_similarity_label_enhanced = CTkLabel(master=app, text="Whisper Similarity (Enhanced): N/A",
+                                                 font=custom_font)
+    whisper_similarity_label_enhanced.place(relx=0.25, rely=0.95, anchor=CENTER)
+
+    vosk_similarity_label_enhanced = CTkLabel(master=app, text="Vosk Similarity (Enhanced): N/A", font=custom_font)
+    vosk_similarity_label_enhanced.place(relx=0.75, rely=0.95, anchor=CENTER)
+    return entry_file_path, vosk_textbox, whisper_textbox, enhanced_file_path
+
+
+if __name__ == "__main__":
+    app = CTk()
+    app.geometry("1200x800")  # Set the window size
+    create_ui(app)
+    app.mainloop()
